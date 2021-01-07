3ddaae7b2d40761a717ff880f49df22b0f676a5b
Gijs Keij <gijs@keij.org>
2021-01-07 17:46:19 +0100

    fixed strings.replace

--- rest-api/main.go
+++ rest-api/main.go
@@ -91,7 +91,7 @@ func Update(w http.ResponseWriter, r *http.Request) {
 	for _, domain := range response.Domains {
 		res1 := strings.Contains(domain, appConfig.Zone)
 		if res1 == true {
-			res2 := strings.Replace(domain, appConfig.Zone, "")
+			res2 := strings.Replace(domain, appConfig.Zone, "", 1)
 			domain = res2
 		}
 		result := UpdateRecord(domain, response.Address, response.AddrType)
